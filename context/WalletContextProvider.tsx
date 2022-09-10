a7141c67504606349c6c2f9186e9f624d997d5ce
john <johnzhengyuliu@gmail.com>
2022-09-10 05:18:47 -0500

    update WalletContextProvider

--- context/WalletContextProvider.tsx
+++ context/WalletContextProvider.tsx
@@ -1,34 +1,30 @@
-import { FC, ReactNode } from "react"
+import { FC, ReactNode, useMemo } from "react"
 import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react"
+import { clusterApiUrl } from "@solana/web3.js"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import * as walletAdapterWallets from "@solana/wallet-adapter-wallets"
-import { AutoConnectProvider, useAutoConnect } from "./AutoConnectProvider"
 require("@solana/wallet-adapter-react-ui/styles.css")
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const { autoConnect } = useAutoConnect()
-  const endpoint = "https://devnet.genesysgo.net/"
-  const wallets = [
-    new walletAdapterWallets.PhantomWalletAdapter(),
-    new walletAdapterWallets.SolflareWalletAdapter(),
-  ]
+  const endpoint = useMemo(() => clusterApiUrl("devnet"), [])
+  const wallets = useMemo(
+    () => [
+      new walletAdapterWallets.PhantomWalletAdapter(),
+      new walletAdapterWallets.SolflareWalletAdapter(),
+    ],
+    []
+  )
 
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect={autoConnect}>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
   )
 }
 
-export const ContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  return (
-    <AutoConnectProvider>
-      <WalletContextProvider>{children}</WalletContextProvider>
-    </AutoConnectProvider>
-  )
-}
+export default WalletContextProvider
