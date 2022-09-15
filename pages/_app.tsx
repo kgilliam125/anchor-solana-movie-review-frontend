28e5e5ae528cd19518110e62ded03ec0e59ade74
john <johnzhengyuliu@gmail.com>
2022-09-15 15:46:05 -0500

    update walletcontxtprovider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
-import { ContextProvider } from "../context/WalletContextProvider"
+import WalletContextProvider from "../context/WalletContextProvider"
 import { WorkspaceProvider } from "../context/Anchor"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ContextProvider>
+    <WalletContextProvider>
       <ChakraProvider>
         <WorkspaceProvider>
           <Component {...pageProps} />
         </WorkspaceProvider>
       </ChakraProvider>
-    </ContextProvider>
+    </WalletContextProvider>
   )
 }
 
